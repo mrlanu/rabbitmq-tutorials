9d2e1f32992934d0466bed2fbdca9c8a86150dbe
mrlanu <mrlanu@gmail.com>
2019-10-08 21:06:35 -0500

    init commit

---
+++ src/main/java/io/lanu/spingboot/rabbitmq/tutorials/RabbitmqTutorialsApplication.java
@@ -0,0 +1,13 @@
+package io.lanu.spingboot.rabbitmq.tutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqTutorialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqTutorialsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/lanu/spingboot/rabbitmq/tutorials/RabbitmqTutorialsApplicationTests.java
@@ -0,0 +1,16 @@
+package io.lanu.spingboot.rabbitmq.tutorials;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitmqTutorialsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
